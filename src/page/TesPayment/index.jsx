f2237e76f88a129efccedf0f7a24527c56573ca0
muhammadisa226 <muhammadisa226@gmail.com>
2023-09-19 22:58:27 +0700

    add test payment

--- src/page/TesPayment/index.jsx
+++ src/page/TesPayment/index.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 const TestPayment = () => {
   const [name, setName] = useState("")
   const [order_id, setOrder_id] = useState("")
-  const [total,setTotal] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [tokenTransaction,setTokenTransaction] = useState("")
   const process = async() => {
         const data = {
             name: name,
@@ -16,15 +17,53 @@ const TestPayment = () => {
     },
   };
     const response = await axios.post(import.meta.env.VITE_BASE_URL + '/user/transaction', data, configg)
-    console.log(response);
+    setTokenTransaction(response.data.token);
+  }
+  useEffect(() => {
+    if (tokenTransaction) {
+      window.snap.pay(tokenTransaction, {
+        onSuccess: (result) => {
+          localStorage.setItem('transaction', JSON.stringify(result))
+          setTokenTransaction("")
+        },
+        onPending: (result) => {
+          localStorage.setItem('transaction', JSON.stringify(result))
+          setTokenTransaction("")
+        },
+        onError: (error) => {
+          console.log(error);
+          setTokenTransaction("")
+        },
+        onClose: () => {
+          console.log('Anda Belum Menyelesaikan Pembayaran');
+          setTokenTransaction("")
+        }
+      })
+      setName("")
+      setOrder_id("")
+      setTotal(0)
     }
+  },[tokenTransaction])
 
+  useEffect(() => {
+    // production use : 'https://app.midtrans.com/snap/snap.js'
+    const midtransUrl = 'https://app.sandbox.midtrans.com/snap/snap.js'
+    let scriptTag = document.createElement("script")
+    scriptTag.src = midtransUrl
+
+    const midtransClientKey = import.meta.env.CLIENT_KEY_MIDTRANS
+    scriptTag.setAttribute("data-client-key", midtransClientKey)
+    document.body.appendChild(scriptTag)
+    return () => {
+      document.body.removeChild(scriptTag)
+    }
+  })
 
 return (
    <div className="relative flex w-full flex-wrap items-stretch mb-3">
   <input type="text" placeholder="name" value={name} onChange={(e) => setName(e.target.value)} className="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white  rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full pr-10"/>
-  <input type="text" placeholder="name" value={order_id} onChange={(e) => setOrder_id(e.target.value)} className="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white  rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full pr-10"/>
-    <input type="text" placeholder="name" value={total} onChange={(e) => setTotal(e.target.value)} className="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white  rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full pr-10" />
+  <input type="text" placeholder="order id" value={order_id} onChange={(e) => setOrder_id(e.target.value)} className="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white  rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full pr-10"/>
+    <input type="number" placeholder="name" value={total} onChange={(e) => setTotal(e.target.value)} className="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white  rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full pr-10" />
     <button className="bg-slate-200  rounded-full p-4 mt-5" onClick={process}>test</button>
   </div>
 
